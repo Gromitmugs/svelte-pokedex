2790e4c303e34894341044173c8ba73cc460d758
Gromitmugs <jediacp2000@gmail.com>
2023-12-26 15:02:31 +0700

    Part 5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,83 @@
 <script lang="ts">
   import type { PageData } from "./$types"; // associates with +page.ts
+  import { generations } from "./generations";
 
   // svelteKit special syntax for retrieving data from load fn
   export let data: PageData;
   // PageData tells the type object by looking at the result from load fn
 </script>
 
-{#each data.monsters as monster (monster.id)}
-  <p>{monster.id}: {monster.name}</p>
-{/each}
+<div class="generations">
+  {#each generations as generation (generation.id)}
+    <div class="generation">{generation.main_region}</div>
+  {/each}
+</div>
+
+<div class="monsters">
+  {#each data.monsters as monster (monster.id)}
+    <div class="monster">
+      <div class="monster-content">
+        <img src={monster.image} alt={monster.name} />
+        {monster.name}
+      </div>
+      <div class="monster-id">
+        {monster.id}
+      </div>
+    </div>
+  {/each}
+</div>
+
+<style>
+  .monsters {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+
+  .monster {
+    width: 100px;
+    margin: 10px;
+    padding: 10px;
+    position: relative;
+    background-color: #eee;
+  }
+
+  .monster:hover {
+    background-color: #ddd;
+  }
+
+  .monster-content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .monster-id {
+    position: absolute;
+    top: 8px;
+    left: 8px;
+    font-size: 0.8em;
+    color: #aaa;
+  }
+
+  .generations {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+
+  .generation {
+    margin: 5px;
+    border: 1px solid;
+    padding: 5px 10px;
+    border: 1px solid black;
+    background-color: #f9f9f9;
+    color: #333;
+  }
+
+  .generation:hover {
+    background-color: #eee;
+  }
+</style>
