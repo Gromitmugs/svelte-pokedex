eb2ae88a49c65cb9f048cb640de00a80ed0580d8
Gromitmugs <jediacp2000@gmail.com>
2023-12-30 15:22:24 +0700

    Part 12

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,50 @@
 <!-- layout page will go in place of everything in every sub-route  -->
-Howdy
+<script lang="ts">
+  import { page } from "$app/stores";
+</script>
 
-<!-- slot includes everything else -->
-<slot />
+<div class="navbar">
+  <a href="/" class:active={$page.url.pathname == "/"}>Home</a>
+  <a href="/my-monsters" class:active={$page.url.pathname == "/my-monsters"}
+    >My Monsters</a
+  >
+</div>
+
+<div class="container">
+  <!-- slot includes everything else -->
+  <slot />
+</div>
+
+<style>
+  .navbar {
+    display: flex;
+    justify-content: space-between;
+    background-color: #333;
+    color: #fff;
+    padding: 10px;
+  }
+
+  .navbar a {
+    color: #fff;
+    text-decoration: none;
+    padding: 4px;
+  }
+  .navbar a:hover {
+    color: #ccc;
+  }
+
+  .navbar a.active {
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+
+  :global(body) {
+    margin: 0;
+  }
+
+  .container {
+    max-width: 90%;
+    margin: 0 auto;
+    width: 1200px;
+  }
+</style>
